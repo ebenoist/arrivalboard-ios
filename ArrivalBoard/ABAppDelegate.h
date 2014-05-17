232492b9bc4d9d8d685f837665854ea8c3220827
Erik Benoist <ebenoist@gmail.com>
2014-05-17 18:24:53 -0500

    Initial Commit

---
+++ ArrivalBoard/ABAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ABAppDelegate.h
+//  ArrivalBoard
+//
+//  Created by Erik Benoist on 5/17/14.
+//  Copyright (c) 2014 eb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ArrivalBoard/ABViewController.h
@@ -0,0 +1,13 @@
+//
+//  ABViewController.h
+//  ArrivalBoard
+//
+//  Created by Erik Benoist on 5/17/14.
+//  Copyright (c) 2014 eb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABViewController : UIViewController
+
+@end
